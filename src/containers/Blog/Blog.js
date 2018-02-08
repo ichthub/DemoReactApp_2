807bca270b125cbeb652175d045904746755a344
ichthub <hassanhoumaid92@gmial.com>
2018-02-08 04:51:40 +0000

    intro to react-router and react-router-dom

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,66 +1,38 @@
 import React, { Component } from 'react';
-//import axios from 'axios';
-import axios from '../../axios'; //my file axios.js
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import { Route, Link } from 'react-router-dom';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
+
 class Blog extends Component {
-	state = {
-		posts: [],
-		selectedPost : null,
-		error : false
-	}
-	componentDidMount () {
-		axios.get('/posts')
-			.then(response => {
-				const slicedPosts = response.data.slice(0, 5);
-				const updatedPosts = slicedPosts.map(post => {
-					return {
-						...post,
-						author: 'Hassan',
-					}
-					
-				});
-				//because http request is asynchronous, we want updating our state after its done
-				this.setState({posts : updatedPosts});//we get data from server
-				//console.log(response);
-			}).catch(error =>{//handle errors
-					//console.log(error);
-					this.setState({error:true});
-				});
-	}
-	selectedPostsHandler = (id) => {
-		this.setState({selectedPost: id});
-	}
 	
     render () {
-		//dynamic data fetched from a query
-		let posts= <p style ={{textAlign: 'center'}}>Something Went Wrong!</p>;
-		if(!this.state.error){
-			posts = this.state.posts.map(post =>{
-				return <Post 
-							key= {post.id} 
-							title = {post.title} 
-							author = {post.author}
-							clicked= {() => {this.selectedPostsHandler(post.id)}}/>;
-							/*this is how you give an argument to a func where you updtae states*/
-			});		
-		
-		}
 		
         return (
-            <div>
-                <section className="Posts">
-                    {posts} {/*myPosts*/}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPost}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+				<header>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/"> Home </Link>
+							</li>
+							<li>{/*add more prop to = ...*/}
+								<Link to={{
+									pathname:'/new_post',
+									hash: '#submit',
+									search : '?quick-submit = true'
+								}}>New Post</Link>
+							</li>
+						</ul>
+					</nav>
+				</header>	
+				{/*messy way to do it by using render */}
+			
+				{/*<Route path = "/" exact render={() => <h1>Home</h1>}/>*/}
+				{/*elegant way to do it using component = {reference here} */}
+				<Route path = "/" exact component={Posts} />
+				<Route path = "/new_post" exact component={NewPost} />
             </div>
         );
     }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'; 
+import axios from '../../../axios';
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+class Posts extends Component {
+	
+	state = {
+		posts: [],
+	}
+		
+	componentDidMount () {
+		console.log(this.props);
+		axios.get('/posts')//async 
+			.then(response => {
+				const slicedPosts = response.data.slice(0, 4);
+				const updatedPosts = slicedPosts.map(post => {
+					return {
+						...post,
+						author: 'Hassan',
+					}
+					
+				});
+				//because http request is asynchronous, we want updating our state after its done
+				this.setState({posts : updatedPosts});//we get data from server
+				//console.log(response);
+			}).catch(error =>{//handle errors
+					console.log(error);
+					//this.setState({error:true});
+				});
+	}
+
+	
+	selectedPostsHandler = (id) => {
+		this.setState({selectedPost: id});
+	}
+	
+   render() {
+	   //dynamic data fetched from a query
+		let posts= <p style ={{textAlign: 'center'}}>Something Went Wrong!</p>;
+		if(!this.state.error){
+			posts = this.state.posts.map(post =>{
+				return <Post 
+							key= {post.id} 
+							title = {post.title} 
+							author = {post.author}
+							clicked= {() => {this.selectedPostsHandler(post.id)}}/>;
+							/*this is how you give an argument to a func where you updtae states*/
+			});		
+
+		}
+      return(
+		<section className="Posts">
+					{posts} {/*myPosts*/}
+		</section>
+      );
+   }
+}
+
+export default Posts;
